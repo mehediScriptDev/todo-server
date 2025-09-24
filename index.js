c12cbdce8bc30030b3036a2fbe103e9e6ee80ba7
Mehedi <mehedi25122003@gmail.com>
2025-09-24 15:19:46 +0600

    dotenv added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -7,13 +8,13 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// s52gKGc43z69nv5n
-// mrtodo
+// 
+// 
 
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
