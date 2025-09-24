40e702ab1bcf4a71b9e3a144e7d915ac4c7d9c20
Mehedi <mehedi25122003@gmail.com>
2025-09-24 14:58:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// s52gKGc43z69nv5n
+// mrtodo
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.wld9ndi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    
+    const listCollections = client.db("todo").collection("lists");
+
+
+    app.post('/todo-lists',async(req,res)=>{
+        const list = req.body;
+        const result = await listCollections.insertOne(list);
+        res.send(result);
+    })
+
+
+
+
+
+
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
